d19de93f49cc774a9905cc2ebdc4591d1cdbcb0a
Jaden Humphries <105307799+Descode8@users.noreply.github.com>
2023-10-08 21:00:55 -0400

    Update ArrayDoubleQueue.java
    Removed if statement for dequeue() ... debugging code

--- src/com/gradescope/DoubleQueue/code/ArrayDoubleQueue.java
+++ src/com/gradescope/DoubleQueue/code/ArrayDoubleQueue.java
@@ -63,11 +63,7 @@ public class ArrayDoubleQueue implements IDoubleQueue
 
     @Override
     public Double dequeue()
-    {	// queue is empty
-	if(front > back){
-		System.out.println("Queue is empty..");
-		return null;
-	}
+    {	
 	Double value = queue[front];
 	queue[front++] = null; // clean up the dequeued element
 	return value;
