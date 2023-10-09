01a19f2d6b4b1eab76c6403597f8c6f403aa5a9f
Jaden Humphries <105307799+Descode8@users.noreply.github.com>
2023-10-08 20:49:26 -0400

    Update IDoubleQueue.java
    Added names

--- src/com/gradescope/DoubleQueue/code/IDoubleQueue.java
+++ src/com/gradescope/DoubleQueue/code/IDoubleQueue.java
@@ -1,5 +1,10 @@
 package com.gradescope.DoubleQueue.code;
-
+/*
+ * Jaden Humphries
+ * Jack Huber
+ * Abby Thorton
+ * Jared Alvarado
+ */
 /**IDoubleQueueContract
  * Queue that contains only Doubles or an empty queue containing ('') characters
  * 
